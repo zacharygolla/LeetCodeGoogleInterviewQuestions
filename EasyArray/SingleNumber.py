8fc5c683ed0ee30cb30431a62d2c897fc924fa9d
zacharygolla <zachary.golla@gmail.com>
2020-07-12 17:30:42 -0500

    add solution for finding single non repeating element in an array full of duplicates

---
+++ EasyArray/SingleNumber.py
@@ -0,0 +1,25 @@
+def main():
+    print(findSingleNumber([2, 3, 2, 3, 5, 6, 1, 6, 1, 5, 8]))
+
+
+def findSingleNumber(nums=[]):
+    # solution using set, time complexity O(n), space complexity(O(n))
+    # dic = set()
+    # for i in range(len(nums)):
+    #     if nums[i] in dic:
+    #         dic.remove(nums[i])
+    #     else:
+    #         dic.add(nums[i])
+
+    # print(dic.pop())
+
+    # solution using bitwise manipulation
+
+    a = 0
+    for i in nums:
+        a ^= i
+    return a
+
+
+if __name__ == "__main__":
+    main()
